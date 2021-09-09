b4f7623df4d5a50166d81ef4e41d0e03805cbbf3
Chethan B <chethan25.bpac@gmail.com>
2021-09-09 16:53:00 +0530

    added error handling for getweatherdata func

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 const searchBtn = document.getElementById('search-btn');
 
-async function getWeatherData() {
-  const APIKEY = 'fcc75fb84b746756bc6f68b8a1268cbf';
-  const searchInput = document
-    .getElementById('search-input')
-    .value.toLowerCase()
-    .trim();
-
-  const weatherData = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${APIKEY}`
-  );
-
-  return weatherData;
+// populate the UI
+function populateUI() {
+  const weatherData = getWeatherData();
+  console.log(weatherData);
 }
 
-searchBtn.addEventListener('click', getWeatherData);
+// get weather data
+async function getWeatherData() {
+  try {
+    const APIKEY = 'fcc75fb84b746756bc6f68b8a1268cbf';
+    const searchInput = document
+      .getElementById('search-input')
+      .value.toLowerCase()
+      .trim();
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${APIKEY}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const weatherData = await response.json();
+
+    return weatherData;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+searchBtn.addEventListener('click', populateUI);
